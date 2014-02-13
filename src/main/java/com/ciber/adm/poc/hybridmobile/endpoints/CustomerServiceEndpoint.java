c3f27ed7a6c6e29b05a34b6d84b1161ab43fa6ed
heiko <heiko.jungholt@gmail.com>
2014-02-13 09:55:59 +0100

    Disabled basic auth and switched one test request to restful url pattern

--- src/main/java/com/ciber/adm/poc/hybridmobile/endpoints/CustomerServiceEndpoint.java
+++ src/main/java/com/ciber/adm/poc/hybridmobile/endpoints/CustomerServiceEndpoint.java
@@ -22,6 +22,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
@@ -41,8 +42,10 @@ public class CustomerServiceEndpoint extends AbstractServiceEndpoint {
     private CustomerService customerService;
 
 	@GET
+//	@Path("{year}/{month}/{day}")
+	@Path("{username}")
 	public Response getCustomerWithUsername(
-			@QueryParam("username") String username) {
+			/*@QueryParam("username")*/ @PathParam("username") String username) {
 		Customer customer = customerService.findCustomerWithUsername(username);
 		if (customer == null)
 			return notFound(username);
